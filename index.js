ac230d0b551b607cc839a951f64e7c5d0214591d
jeanleeb <jeanleeb@outlook.com.br>
2023-01-17 15:42:46 -0300

    remove comments

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var { buildSchema } = require("graphql");
 const expressPlayground =
   require("graphql-playground-middleware-express").default;
 
-// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
-// The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
     return "Hello world!";

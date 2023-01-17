9a81bf9473c1fbd30e773f8d965b34da0c078388
jeanleeb <jeanleeb@outlook.com.br>
2023-01-17 19:29:09 -0300

    add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+  root: true,
+  ignorePatterns: ['node_modules', 'dist'],
+};

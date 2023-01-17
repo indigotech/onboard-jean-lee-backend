6ff1b6c8d79c1f0df20a4cbc766647c5a6a3c4f4
jeanleeb <jeanleeb@outlook.com.br>
2023-01-17 15:37:57 -0300

    setup graphql server

---
+++ README.md
@@ -0,0 +1,11 @@
+# Onboarding Backend
+
+Server capable of storing data in a database and CRUDing it, developed as part of the Taqtile backend onboarding.
+
+## Environment and tools
+
+TODO
+
+## Running and debugging
+
+TODO
---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+const expressPlayground =
+  require("graphql-playground-middleware-express").default;
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+  })
+);
+app.get('/', expressPlayground({ endpoint: '/graphql' }));
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");

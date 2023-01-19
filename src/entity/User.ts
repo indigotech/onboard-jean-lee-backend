04d7a2312aefdfd5022bfa1e0013881dac79b389
jeanleeb <jeanleeb@outlook.com.br>
2023-01-19 13:26:45 -0300

    change user id to number, simplify user creation

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
 
   @Column()
   name: string;
--- src/resolver.ts
+++ src/resolver.ts
@@ -7,17 +7,9 @@ export const resolvers = {
   },
   Mutation: {
     createUser: async (parent, args) => {
-      const input = args.input;
-      const user = new User();
-      user.name = input.name;
-      user.email = input.email;
-      user.password = input.password;
-      user.birthDate = input.birthDate;
+      const user = Object.assign(new User(), args.input);
 
       await AppDataSource.manager.save(user);
-      console.log('Saved a new user with id: ' + user.id);
-
-      delete user.password;
 
       return user;
     },

a00c09b3a4e4ffdfaa782606006835de29858d38
jeanleeb <jeanleeb@outlook.com.br>
2023-01-19 18:14:00 -0300

    add chai assertion

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import { initializeServer } from '..';
+import { expect } from 'chai';
 
-describe('hello world', () => {
+describe('test hello query', () => {
   before('starting server', async () => {
     await initializeServer();
   });
 
-  it('will say hello', async () => {
+  it('should get the correct response for the hello query', async () => {
     const response = await axios.post(`http://localhost:4000`, { query: 'query { hello }' });
-    console.log(response.data);
+    expect(response.data).to.be.deep.equal({ data: { hello: 'Hello world!' } });
   });
 });
